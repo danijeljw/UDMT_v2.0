6aa8a636d9d9eb09ad43e96c6652d48447cf7fc7
Danijel-James Wynyard <danijel.james@me.com>
2017-08-03 16:16:47 +0930

    finalised files not copied code

--- functions/function_FileNamesNotCopiedTooLong.ps1
+++ functions/function_FileNamesNotCopiedTooLong.ps1
@@ -7,7 +7,61 @@
 [CARRIED] $global:DirectoryComposite
 #>
 
+$global:TooLongFileNamesUserData = $NULL
+$global:TooLongFileNamesUsersUserID = $NULL
 
+function global:FileNamesNotCopiedTooLong{
+    "*****Files from C:\UserData*****" | Out-File "$global:TargetPath\FilesNotCopied.txt" -Append
+    cmd /c "dir /b /s /a:d C:\UserData" | ForEach-Object{
+        If ( $_.length -gt 250 ){
+            $_ | Out-File "$global:TargetPath\FilesNotCopied.txt"
+        }
+    "*****Files from C:\Users\$env:USERNAME\Contacts*****" | Out-File "$global:TargetPath\FilesNotCopied.txt" -Append
+    cmd /c "dir /b /s /a:d C:\UserData\$env:USERNAME\Contacts" | ForEach-Object{
+        If ( $_.length -gt 250){
+            $_ | Out-File "$global:TargetPath\FilesNotCopied.txt"
+        }
+    "*****Files from C:\Users\$env:USERNAME\Desktop*****" | Out-File "$global:TargetPath\FilesNotCopied.txt" -Append
+    cmd /c "dir /b /s /a:d C:\UserData\$env:USERNAME\Desktop" | ForEach-Object{
+        If ( $_.length -gt 250){
+            $_ | Out-File "$global:TargetPath\FilesNotCopied.txt"
+        }
+    "*****Files from C:\Users\$env:USERNAME\Documents*****" | Out-File "$global:TargetPath\FilesNotCopied.txt" -Append
+    cmd /c "dir /b /s /a:d C:\UserData\$env:USERNAME\Documents" | ForEach-Object{
+        If ( $_.length -gt 250){
+            $_ | Out-File "$global:TargetPath\FilesNotCopied.txt"
+        }
+    "*****Files from C:\Users\$env:USERNAME\Downloads*****" | Out-File "$global:TargetPath\FilesNotCopied.txt" -Append
+    cmd /c "dir /b /s /a:d C:\UserData\$env:USERNAME\Downloads" | ForEach-Object{
+        If ( $_.length -gt 250){
+            $_ | Out-File "$global:TargetPath\FilesNotCopied.txt"
+        }
+    "*****Files from C:\Users\$env:USERNAME\Favourites*****" | Out-File "$global:TargetPath\FilesNotCopied.txt" -Append
+    cmd /c "dir /b /s /a:d C:\UserData\$env:USERNAME\Favourites" | ForEach-Object{
+        If ( $_.length -gt 250){
+            $_ | Out-File "$global:TargetPath\FilesNotCopied.txt"
+        }
+    "*****Files from C:\Users\$env:USERNAME\Links*****" | Out-File "$global:TargetPath\FilesNotCopied.txt" -Append
+    cmd /c "dir /b /s /a:d C:\UserData\$env:USERNAME\Links" | ForEach-Object{
+        If ( $_.length -gt 250){
+            $_ | Out-File "$global:TargetPath\FilesNotCopied.txt"
+        }
+    "*****Files from C:\Users\$env:USERNAME\Music*****" | Out-File "$global:TargetPath\FilesNotCopied.txt" -Append
+    cmd /c "dir /b /s /a:d C:\UserData\$env:USERNAME\Music" | ForEach-Object{
+        If ( $_.length -gt 250){
+            $_ | Out-File "$global:TargetPath\FilesNotCopied.txt"
+        }
+    "*****Files from C:\Users\$env:USERNAME\Pictures*****" | Out-File "$global:TargetPath\FilesNotCopied.txt" -Append
+    cmd /c "dir /b /s /a:d C:\UserData\$env:USERNAME\Pictures" | ForEach-Object{
+        If ( $_.length -gt 250){
+            $_ | Out-File "$global:TargetPath\FilesNotCopied.txt"
+        }
+    "*****Files from C:\Users\$env:USERNAME\Searches*****" | Out-File "$global:TargetPath\FilesNotCopied.txt" -Append
+    cmd /c "dir /b /s /a:d C:\UserData\$env:USERNAME\Searches" | ForEach-Object{
+        If ( $_.length -gt 250){
+            $_ | Out-File "$global:TargetPath\FilesNotCopied.txt"
+        }
+}
 
 
 
