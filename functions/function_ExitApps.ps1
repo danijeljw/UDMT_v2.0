950432647a40178c59bd4b7ded971d0c448acc0f
Danijel-James Wynyard <danijel.james@me.com>
2017-08-03 15:04:40 +0930

    create _ExitApps complete

---
+++ functions/function_ExitApps.ps1
@@ -0,0 +1,22 @@
+﻿<#
+[FROM] _ExportOutlookSettings
+#>
+
+<#
+[CARRIED] $global:TargetPath
+[CARRIED] $global:DirectoryComposite
+#>
+
+# List of apps that need to be closed if they're currently open as an array
+$global:CloseOpenApps = @("outlook","firefox","chrome","iexplore","winword","powerpnt","onenote","excel")
+
+Get-Process $global:CloseOpenApps | ForEach-Object {$_.CloseMainWindow() }
+
+<#
+[CARRIED] $global:TargetPath
+[CARRIED] $global:DirectoryComposite
+#>
+
+<#
+[GOTO] _FileNamesNotCopiedTooLong
+#>
