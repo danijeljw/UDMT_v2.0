a9850f808f61f849822746162138d6c6f6dd048a
Danijel-James Wynyard <danijel.james@protonmail.ch>
2017-08-03 21:38:28 +1000

    export network printers to file

---
+++ functions/function_ExportNetworkPrinters.ps1
@@ -0,0 +1,17 @@
+﻿<#
+[FROM] ExportNetworkDrives
+#>
+
+
+<#
+[CARRIED] $global:TargetPath
+[CARRIED] $global:DirectoryComposite
+#>
+
+function global:ExportNetworkPrinters{
+    Get-WMIObject Win32_Printer -ComputerName $env:COMPUTERNAME | where{$_.Name -like “*\\*”} | select sharename,name | Out-File "$global:TargetPath\Printers.txt" -Append
+}
+
+<#
+[GOTO] Done
+#>
