6af284ffe7af499e2adf5bb01ac7346ccfb29093
Danijel-James Wynyard <danijel.james@protonmail.ch>
2017-08-03 21:38:18 +1000

    create backup files and folders

---
+++ functions/function_BackupFilesAndFolders.ps1
@@ -0,0 +1,32 @@
+﻿<#
+[FROM] FileNamesNotCopiedTooLong
+#>
+
+
+<#
+[CARRIED] $global:TargetPath
+[CARRIED] $global:DirectoryComposite
+#>
+
+
+function global:BackupFilesAndFolders{
+    # Backup UserData folder
+    Write-Host "Action 1 of Xc" -ForegroundColor Green
+    Write-Host "Copying UserData folder"
+    Copy-Item C:\UserData\* $global:BasePath\UserData -Recurse -Force
+
+    # Backup $DirectoryComposite folders
+    ForEach ( $r in $global:DirectoryComposite ){
+        Write-Host "Copying $r folder"
+        Copy-Item "$env:USERPROFILE\$r\*" "$global:BasePath\$r" -Recurse -Force
+    }
+}
+
+<#
+[CARRIED] $global:TargetPath
+[CARRIED] $global:DirectoryComposite
+#>
+
+<#
+[GOTO] _ExportNetworkDrives
+#>
