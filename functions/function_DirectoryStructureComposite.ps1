756fdca1ed7b1ace68132a89b433e6b4ceb73856
Danijel-James Wynyard <danijel.james@protonmail.ch>
2017-10-18 08:34:06 +1030

    code cleanup

--- functions/function_DirectoryStructureComposite.ps1
+++ functions/function_DirectoryStructureComposite.ps1
@@ -23,43 +23,49 @@ $Global:DirectoryComposite = @("Contacts",
 
 function global:DirectoryStructureComposite{
     If ( Test-Path $Global:TargetPath ) {
+
         # beeps to alert user
         [console]::beep(1000,500)
         [console]::beep(2000,500)
         [console]::beep(1000,500)
+
         # Path for current machine exists, confirm to overwrite with the word "YES"
         [void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
         $ContinueYes = [Microsoft.VisualBasic.Interaction]::InputBox("Backup of " + $env:COMPUTERNAME + " already exists.`n`nType 'YES' into box to continue", "Backup Directory Exists", "YES")
-        }
-
-        # If "YES" or "yes" is not typed in, will exit here, using regex
-        $YES_Only = "^[esyESY]+$"
-        If (-not( $ContinueYes -match $YES_Only)){
-            [console]::beep(1000,500)
-            [console]::beep(1000,500)
-            [System.Windows.Forms.MessageBox]::Show("Backup aborted.`n`n'YES' was not entered in previous box.`n`n`nError 1002","User Request Exit")
-            Exit
-        }
-
-    } Else {
-        # Make the C:\UserData folder on the USB drive
-        New-Item $Global:TargetPath\UserData -ItemType Directory -Force
-
-        # Create an Outlook signatures folder on the USB drive
-        New-Item $Global:TargetPath\Users\$env:USERNAME\Roaming\Microsoft\Signatures -ItemType Directory -Force
-
-        # Create Google Chrome bookmarks folder on the USB drive
-        New-Item "$Global:TargetPath\Users\$env:USERNAME\AppData\Local\Google\Chrome\User Data\Default"
-    
-        # Using folders listed in the array $Global:DirectoryComposite, create each folder
-        ForEach ( $x in $Global:DirectoryComposite ){
-            New-Item $Global:BasePath\Users\$env:USERNAME\$x -ItemType Directory -Force
-        }
     }
 
-    # Cleanup no-longer required variables
-    Remove-Variable -Name CurrentYearOnly -Scope Global -Force
+    # If "YES" or "yes" is not typed in, will exit here, using regex
+    $YES_Only = "^[esyESY]+$"
+    If (-not( $ContinueYes -match $YES_Only)){
+        [console]::beep(1000,500)
+        [console]::beep(1000,500)
+        [System.Windows.Forms.MessageBox]::Show("Backup aborted.`n`n'YES' was not entered in previous box.`n`n`nError 1002","User Request Exit")
+        Exit
+    }
+
 }
+
+# Call functions
+global:DirectoryStructureComposite
+
+# Make the C:\UserData folder on the USB drive
+New-Item $Global:TargetPath\UserData -ItemType Directory -Force
+
+# Create an Outlook signatures folder on the USB drive
+New-Item $Global:TargetPath\Users\$env:USERNAME\Roaming\Microsoft\Signatures -ItemType Directory -Force
+
+# Create Google Chrome bookmarks folder on the USB drive
+New-Item "$Global:TargetPath\Users\$env:USERNAME\AppData\Local\Google\Chrome\User Data\Default"
+    
+# Using folders listed in the array $Global:DirectoryComposite, create each folder
+ForEach ( $x in $Global:DirectoryComposite ){
+    New-Item $Global:BasePath\Users\$env:USERNAME\$x -ItemType Directory -Force
+    
+}
+
+# Cleanup no-longer required variables
+Remove-Variable -Name CurrentYearOnly -Scope Global -Force
+
 
 
 <#
