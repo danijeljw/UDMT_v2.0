a5f97c82fb471fdcb3068b991d3c817fe15df2ec
Danijel-James Wynyard <danijel.james@protonmail.ch>
2017-10-19 13:44:44 +1030

    major changes

--- mainScript.ps1
+++ mainScript.ps1
@@ -23,23 +23,44 @@
 
 QueryUSBDriveConnected
 
-# Prompt with list of apps that are required to be closed
-[void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
-$Global:Apps2Shutdown = [Microsoft.VisualBasic.Interaction]::InputBox("The following applications will be closed automatically:`n`nWord, Excel, Outlook, PowerPoint, OneNote, Internet Explorer, Google Chrome`n`n`nType 'YES' into the box and click OK to confirm.", "Closing Mandatory Apps", "No")
 
-# If not OK to close these applications, exit program
-$YES_Only = "^[esyESY]+$"
-If (-not( $Global:Apps2Shutdown -match $YES_Only)){
-    [console]::beep(1000,500)
-    [console]::beep(1000,500)
-    [System.Windows.Forms.MessageBox]::Show("Backup aborted.`n`n'YES' was not entered in previous box.`n`n`nError G404","User Request Exit")
-    Exit
+
+<#
+==============================================================================
+#>
+
+
+
+function Global:WarningCloseApps{
+    # Prompt with list of apps that are required to be closed
+    [void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
+    $Global:Apps2Shutdown = [Microsoft.VisualBasic.Interaction]::InputBox("The following applications will be closed automatically:`n`nWord, Excel, Outlook, PowerPoint, OneNote, Internet Explorer, Google Chrome`n`n`nType 'YES' into the box and click OK to confirm.", "Closing Mandatory Apps", "No")
+
+    # If not OK to close these applications, exit program
+    $YES_Only = "^[esyESY]+$"
+    If (-not( $Global:Apps2Shutdown -match $YES_Only)){
+        [console]::beep(1000,500)
+        [console]::beep(1000,500)
+        [System.Windows.Forms.MessageBox]::Show("Backup aborted.`n`n'YES' was not entered in previous box.`n`n`nError G404","User Request Exit")
+        Exit
+    }
 }
 
-function global:ShowConnectedDrives{
+WarningCloseApps
+
+
+
+<#
+==============================================================================
+#>
+
+
+
+function global:ShowConnectedDrivesAndSelectDrive{
 
     # Clear the screen of text first
     Clear-Host
+
     "The drives you currently have connected are:" | Out-File C:\Temp\listofdrives.txt
     Get-WmiObject Win32_DiskDrive | % {
       $disk = $_
@@ -66,7 +87,7 @@ function global:ShowConnectedDrives{
         }
       }
     } | Out-File C:\Temp\listofdrives.txt -Append
-
+    # Open the TXT file with the apps
     Start-Process -FilePath "listofdrives.txt" -WorkingDirectory "C:\Temp" 
 
 
@@ -94,39 +115,14 @@ function global:ShowConnectedDrives{
 
     # Let the user know backup will commence to the drive X they selected
     [System.Windows.Forms.MessageBox]::Show("The USB drive you selected is " + $Global:SelectedDrive + ".`nDo not disconnect the drive until process finalised.`n`nThis process can take between 15-60 minutes to complete!","Data Backup Start",0) 
+
+    .{
+        Stop-Process -Name notepad -ErrorAction SilentlyContinue 
+    }
 }
 
-ShowConnectedDrives
+ShowConnectedDrivesAndSelectDrive
 
-function global:DriveSelected{
-
-    # Wait 3 seconds before firing up
-    Start-Sleep -s 3
-    [void][System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic')
-
-    # $Global:SelectedDrive variable is set to letter that carries through rest of script now
-    $Global:SelectedDrive = [Microsoft.VisualBasic.Interaction]::InputBox("From the drives listed in the blue window, type just the LETTER of the drive from the list you want to back up to and click on the OK button.", "Drive Letter Selection", "X")
-    
-    # Regex to test drive letter is only [D, E, F, H, I, J, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]
-    $A2Z_Only = "^[d-fh-jl-zD-FH-JL-Z]+$"
-
-    # Ensure that ONLY a drive letter is provided, else exit
-    If($Global:SelectedDrive.length -gt 1){
-        [System.Windows.Forms.MessageBox]::Show($Global:SelectedDrive + " is not just the drive letter`n`nExiting app.`n`n`nError Code: D21","Not Just Drive Letter Provided")
-        Exit
-    }
-
-    # Match drive letter provided is as per regex test, else exit
-    If(-Not($Global:SelectedDrive -match $A2Z_Only)){
-        [System.Windows.Forms.MessageBox]::Show($Global:SelectedDrive + " is not a valid drive letter`n`n`nError Code: A1045","Incorrect Drive Letter Provided")
-        Exit
-    }
-
-    # Let the user know backup will commence to the drive X they selected
-    [System.Windows.Forms.MessageBox]::Show("The USB drive you selected is " + $Global:SelectedDrive + ".`nDo not disconnect the drive until process finalised.`n`nThis process can take between 15-60 minutes to complete!","Data Backup Start",0) 
-}
-
-DriveSelected
 
 $Global:CurrentYearOnly = (Get-Date).Year
 
